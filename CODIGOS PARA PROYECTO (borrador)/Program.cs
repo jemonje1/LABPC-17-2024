5f8fb7ffce9d9184f3a4823b71f60e65f1677b23
jemonje1 <157499614+jemonje1@users.noreply.github.com>
2024-05-19 22:09:59 -0600

    Add files via upload

--- CODIGOS PARA PROYECTO (borrador)/Program.cs
+++ CODIGOS PARA PROYECTO (borrador)/Program.cs
@@ -1,126 +1,252 @@
-﻿using System.Security.Cryptography.X509Certificates;
-
-class Program
+﻿namespace CountryBank
 {
-    public static void Main(string[] args)
+    class Program
     {
-        Nombres();
-        Notas();
-        Menu();
-    }
-
-    static string[] nombres = new string[10];
-    public static void Nombres()
-    {
-        for (int i = 0; i < nombres.Length; i++)
+        static string tipoCuenta = ""; 
+        static string nombreCuentahabitante = "";
+        static string DPI = "";
+        static string direccion = "";
+        static string numeroTelefono = "";
+        static double saldoActualCuenta = 2500.00;
+        static int abono = 0;
+        static int tiempoSimulador = 0;
+        static string opciones = "";
+        static int transaccion = 0;
+        static string servicioAgua = "Servicio de Agua";
+        static string servicioTelefono = "Servicio de Teléfono";
+        static string servicioElectrico="Servicio Eléctrico";
+        static bool salidaOtrosMenus = false;
+        static int NumeroArray = 0;
+        static void Main(string[] args)
         {
-            System.Console.WriteLine("\n______________________");
-            System.Console.WriteLine("Ingrese un nombre");
-            string name = Console.ReadLine();
-            nombres[i] = name;
-        }
-    }
-
-    static int[] notas = new int[10];
-    public static void Notas()
-    {
-        for (int i = 0; i < notas.Length; i++)
-        {
-            bool validacion = false;
-            do
-            {
-                try
-                {
-                    System.Console.WriteLine("\n______________________");
-                    System.Console.WriteLine($"Ingrese nota {i}");
-                    int nota = Int32.Parse(Console.ReadLine());
-                    notas[i] = nota;
-                    validacion = false;
-                }
-                catch (FormatException e)
-                {
-                    System.Console.WriteLine("Error! dato ingresado no valido\nINTENTELO DE NUEVO");
-                    System.Console.WriteLine(e.Message);
-                    validacion = true;
-                }
-            } while (validacion);
-        }
-    }
-
-    public static void Menu()
-    {
-        bool validacion = true;
-        do
-        {
-            System.Console.WriteLine("\n___________MENU___________");
-            System.Console.WriteLine("1.Mostrar nombre y nota de alumnos que aprobaron el curso.\n2.Mostrar nombre y nota de alumnos que No aprobaron el curso.\n3.Mostrar el promedio de notas del grupo.\n4.Salir del programa");
-            string opcion = Console.ReadLine();
-
-            switch (opcion)
+            Program Banco = new Program();
+            Console.WriteLine("------------------------------------------------");
+            Console.WriteLine("Bienvenido a la banca virtual del Banco Country");
+            Console.WriteLine("------------------------------------------------");
+            bool Salir = false;
+            Console.WriteLine("Ingrese su información");
+            Console.WriteLine($"Ingrese su tipo de cuenta \n a. Monetaria en Q \n b. Ahorro en Q \n c. Monetaria en $ \n d. Ahorro en $");
+            switch (opciones)
             {
                 case "1":
-                    {
-                        System.Console.WriteLine("\n--------------------");
-                        System.Console.WriteLine("Las notas que aprobaron son:");
-                        Ganadores();
-                        System.Console.WriteLine("--------------------");
-                        break;
-                    }
+                    tipoCuenta = "Monetaria en Q";
+                    break;
                 case "2":
-                    {
-                        System.Console.WriteLine("\n--------------------");
-                        System.Console.WriteLine("Las notas que aprobaron son:");
-                        Perdedores();
-                        System.Console.WriteLine("--------------------");
-                        break;
-                    }
+                    tipoCuenta = "Ahorro en Q";
+                    break;
                 case "3":
-                    {
-                        System.Console.WriteLine("\n--------------------");
-                        System.Console.WriteLine("El promedio es " + Promedio());
-                        System.Console.WriteLine("--------------------");
-                        break;
-                    }
+                    tipoCuenta = "Monetaria en $";
+                    break;
                 case "4":
-                    {
-                        validacion = false;
-                        break;
-                    }
+                    tipoCuenta = "Ahorro en $";
+                    break;
                 default:
-                    {
-                        System.Console.WriteLine("Opcion no valida intentelo de nuevo");
-                        break;
-                    }
+                    Console.WriteLine("Porfavor elija una cuenta");
+                    break;
             }
-        } while (validacion);
-    }
-
-    public static int Promedio()
-    {
-        int promedio = 0;
-        for (int i = 0; i < notas.Length; i++)
-        {
-            promedio = promedio + notas[i];
-        }
-        promedio = promedio / notas.Length;
-        return promedio;
-    }
-
-    public static void Ganadores(){
-        for (int i = 0; i < notas.Length; i++)
-        {
-            if (notas[i] >= 65)
+            if (tipoCuenta == "Monetaria en Q" || tipoCuenta == "Ahorro en Q" || tipoCuenta == "Monetaria en $" || tipoCuenta == "Ahorro en $")
             {
-                System.Console.WriteLine(notas[i]);
+                Console.WriteLine("");
+                Console.WriteLine("Ingrese su nombre");
+                nombreCuentahabitante = Console.ReadLine();
+                Console.WriteLine("");
+                Console.WriteLine("Ingrese su DPI (5 caracteres)");
+                DPI = Console.ReadLine();
+
+                int leng = DPI.Length;
+                do
+                {
+                    Console.WriteLine("Por favor ingrese su DPI (5 caracteres)");
+                    DPI = Console.ReadLine();
+                    leng = DPI.Length;
+                }
+                while (leng < 5 || leng > 5);
+                Console.WriteLine("");
+                Console.WriteLine("Ingrese su dirección");
+                direccion = Console.ReadLine();
+                Console.WriteLine("");
+                Console.WriteLine("Ingrese su número de teléfono");
+                numeroTelefono = Console.ReadLine();
+            do
+            {
+                Console.WriteLine(" ");
+                Console.WriteLine("Seleccione el número de la opción que desea ejecutar");
+                Console.WriteLine("1. Ingresar a cuenta");
+                Console.WriteLine("2. Comprar Producto");
+                Console.WriteLine("3. Vender Producto");
+                Console.WriteLine("4. Abonar a cuenta");
+                Console.WriteLine("5. Simular paso del tiempo");
+                Console.WriteLine("6. Cuentas de terceros");
+                switch (opciones)
+                {
+                    case "1":
+                        {
+                            Console.WriteLine($"Nombre: {nombreCuentahabitante}");
+                            Console.WriteLine($"Tipo Cuenta: {tipoCuenta}");
+                            Console.WriteLine($"DPI: {DPI}");
+                            Console.WriteLine($"Número de telefono: {numeroTelefono}");
+                            Console.WriteLine($"Dirección: {direccion}");
+                            Console.WriteLine($"Saldo Actual: {saldoActualCuenta}");
+                            break;
+                        }
+                    case "2":
+                        {
+                            Banco.CaseII();
+                            break;
+                        }
+                    case "3":
+                        {
+                            Banco.CaseIII();
+                            break;
+                        }
+                    case "4":
+                        {
+                            Banco.caseIV();
+                            break;
+                        }
+                    case "5":
+                        {
+                            Banco.CaseV();
+                            break;
+                        }
+                    case "6":
+                        {
+                            Banco.CaseVI();
+                            break;
+                        }
+                    case "8":
+                        {
+                            Banco.CaseVIII();
+                            break;
+                        }
+                    default:
+                        {
+                            Console.WriteLine("Opción no valida, seleccionar otra opción");
+                            break;
+                        }
+                }
+            }
+            while (!Salir);
+            }
+            else
+            {
+                Console.WriteLine("Su tipo de cuenta no existe");
             }
         }
-    }
-    public static void Perdedores(){
-        for (int i = 0; i < notas.Length; i++)
+        public void CaseII()
         {
-            if (notas[i] < 65)
+            saldoActualCuenta = saldoActualCuenta - (saldoActualCuenta * 0.1);
+            Console.WriteLine ($"Su saldo actual es de: {saldoActualCuenta}");
+        }
+         public void CaseIII()
+        {
+            if( saldoActualCuenta > 500)
             {
-                System.Console.WriteLine(notas[i]);
+                saldoActualCuenta = saldoActualCuenta + (saldoActualCuenta * 0.11);
+                Console.WriteLine($"Su saldo actual es de: {saldoActualCuenta}");
+            }
+            else
+            {
+                double g = saldoActualCuenta/100;
+                Console.WriteLine("Se recomienda no realizar la transacción debido a que el porcentaje de su saldo actual es " + g + "%");
+            }
+        }
+        public void caseIV()
+        {
+            Console.WriteLine("");
+            if (abono < 2 && saldoActualCuenta >  500)
+            {
+                saldoActualCuenta = saldoActualCuenta * 2;
+                abono++;
+                Console.WriteLine($"Abono realizado con exito, su saldo actual es {saldoActualCuenta}");
+            }
+            else
+            {
+                Console.WriteLine("Imposible realizar el abono");
+            }
+        }
+        public void CaseV()
+        {
+            int simulador = 0;
+            int suma = 0;
+            Console.WriteLine("Simular el paso del tiempo");
+            Console.WriteLine($"¿Cómo desea simular el tiempo? \n 1. En meses \n 2. En días");
+            switch (opciones)
+            {
+                case "1":
+                    Console.WriteLine("Ingrese su tiempo de simulación en meses");
+                    tiempoSimulador = int.Parse(Console.ReadLine());
+                    simulador = (int)(double)(saldoActualCuenta * 0.02 * tiempoSimulador)/12;
+                    Console.WriteLine("El interés es de: " + simulador);
+                    suma = (int)(simulador + saldoActualCuenta);
+                    Console.WriteLine($"Su cuenta sería de {suma} con los meses ingresados");
+                    break;
+
+                case "2":
+                    simulador = (int)(double)(saldoActualCuenta * 0.02 * tiempoSimulador)/360;
+                    Console.WriteLine("El interés es de: " + simulador);
+                    suma = (int)(simulador + saldoActualCuenta);
+                    Console.WriteLine($"Su cuenta sería de {suma} con los días ingresados");
+                    break;
+
+                default:
+                    Console.WriteLine("La opción que escogió, no existe");
+                    break;
+            }
+        }
+        public void CaseVI()
+        {
+            Console.WriteLine($"Bienvenido al menú de mantenimiento de cuentas de terceros");
+            Console.WriteLine($"Indique la opción que desea realizar \n 1. Crear nueva cuenta tercera \n 2. Eliminar cuenta \n 3. Modificar cuenta");
+            switch(opciones)
+            {
+                case "1":
+                break;
+                case "2":
+                break;
+                case "3":
+                break;
+                default:
+                break;
+            }
+        }
+        public void CaseVII()
+        {
+            Console.WriteLine("Transacciones");
+        }
+        public void CaseVIII()
+        {
+            Console.WriteLine("Pago de servicios");
+            Console.WriteLine("Indique a cuál servicio desea pagar \n 1. Servicio agua \n 2. Servicio eléctrico \n 3. Servicio de teléfono");
+            switch (opciones)
+            {
+                case "1":
+                Console.WriteLine($"Ingresó al {servicioAgua}");
+                Console.WriteLine("Ingrese el monto que quiera pagar");
+                transaccion = int.Parse(Console.ReadLine());
+                saldoActualCuenta = saldoActualCuenta - transaccion;
+                Console.WriteLine($"Realizó una transacción de {transaccion}");
+                Console.WriteLine($"Su saldo actual es de {saldoActualCuenta}");
+                break;
+                case "2":
+                Console.WriteLine($"Ingresó al {servicioElectrico}");
+                Console.WriteLine("Ingrese el monto que quiera pagar");
+                transaccion = int.Parse(Console.ReadLine());
+                saldoActualCuenta = saldoActualCuenta - transaccion;
+                Console.WriteLine($"Realizó una transacción de {transaccion}");
+                Console.WriteLine($"Su saldo actual es de {saldoActualCuenta}");
+                break;
+                case "3":
+                Console.WriteLine($"Ingresó al {servicioTelefono}");
+                Console.WriteLine("Ingrese el monto que quiera pagar");
+                transaccion = int.Parse(Console.ReadLine());
+                saldoActualCuenta = saldoActualCuenta - transaccion;
+                Console.WriteLine($"Realizó una transacción de {transaccion}");
+                Console.WriteLine($"Su saldo actual es de {saldoActualCuenta}");
+                break;
+                default:
+                Console.WriteLine("La opción que ingresó no es válida, intente nuevamente");
+                break;
             }
         }
     }
